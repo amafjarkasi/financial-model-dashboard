1947c574a670f6f6417df678bd22ed187d9ac383
Leslie Wen <leslie.wen@gmail.com>
2020-12-05 11:11:53 -0500

    Fixed date

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,23 +7,17 @@ class Navbar extends Component {
   
   componentDidMount(){
     let dateNow=new Date();
-    
-    if(dateNow.getMinutes()<10){
+    if(parseInt(dateNow.getMinutes())<10){
       this.setState({currentDate:dateNow.getMonth()+1+"/"+dateNow.getDate()+"/"+dateNow.getFullYear()+" @ "+dateNow.getHours()+":0"+dateNow.getMinutes()});
     }
-
     this.setState({currentDate:dateNow.getMonth()+1+"/"+dateNow.getDate()+"/"+dateNow.getFullYear()+" @ "+dateNow.getHours()+":"+dateNow.getMinutes()});
-    console.log(this.currentDate)
   }
-
-
-
   render() {
     return (
       <nav className="navbar navbar-expand-lg " color-on-scroll="500">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Financial Market Dashboard</a>
-          <a className="navbar-brand" href="/">&#x2800;&#x2800;&#x2800;&#x2800;&#x2800;Date:{this.currentDate}</a>
+          <a className="navbar-brand" href="/">&#x2800;&#x2800;&#x2800;&#x2800;&#x2800;Date:{this.state.currentDate}</a>
           <div className="collapse navbar-collapse justify-content-end" id="navigation">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
