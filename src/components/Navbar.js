3a1052dba1849ee9797ee695e80ab9d3b09ab4c1
Leslie Wen <leslie.wen@gmail.com>
2020-12-07 10:07:44 -0500

    Fixed leading zero

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,18 @@ class Navbar extends Component {
   
   componentDidMount(){
     let dateNow=new Date();
-    if(parseInt(dateNow.getMinutes())<10){
+    if((dateNow.getMinutes()<10)===true){
       this.setState({currentDate:dateNow.getMonth()+1+"/"+dateNow.getDate()+"/"+dateNow.getFullYear()+" @ "+dateNow.getHours()+":0"+dateNow.getMinutes()});
+    }else{
+      this.setState({currentDate:dateNow.getMonth()+1+"/"+dateNow.getDate()+"/"+dateNow.getFullYear()+" @ "+dateNow.getHours()+":"+dateNow.getMinutes()});
     }
-    this.setState({currentDate:dateNow.getMonth()+1+"/"+dateNow.getDate()+"/"+dateNow.getFullYear()+" @ "+dateNow.getHours()+":"+dateNow.getMinutes()});
   }
   render() {
     return (
       <nav className="navbar navbar-expand-lg " color-on-scroll="500">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Financial Market Dashboard</a>
-          <a className="navbar-brand" href="/">&#x2800;&#x2800;&#x2800;&#x2800;&#x2800;Date:{this.state.currentDate}</a>
+          <a className="navbar-brand" href="/">&#x2800;&#x2800;&#x2800;&#x2800;&#x2800;Date: {this.state.currentDate}</a>
           <div className="collapse navbar-collapse justify-content-end" id="navigation">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
